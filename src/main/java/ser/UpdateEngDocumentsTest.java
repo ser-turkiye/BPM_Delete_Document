a141a90523ebdbebb00c8f5579de1ecb342c3e7a
bbayhan <bulentbayhan@gmail.com>
2024-05-07 15:47:21 +0300

    EngDocuments da DOCID alanın doldurulması için batch update eklendi

--- src/main/java/ser/UpdateEngDocumentsTest.java
+++ src/main/java/ser/UpdateEngDocumentsTest.java
@@ -30,56 +30,59 @@ public class UpdateEngDocumentsTest extends UnifiedAgent {
         Utils.session = getSes();
         Utils.bpm = getBpm();
         Utils.server = Utils.session.getDocumentServer();
-
         try {
             helper = new ProcessHelper(getSes());
-            log.info("-- Delete MultiReview Process Agent Started -----");
-            IInformationObject[] list = this.getEngineeringDocs(helper);
-            for(IInformationObject infoDoc : list){
-                ILink[] links2 = getDocumentServer().getReferencingRelationships(getSes(),((IDocument) infoDoc).getID(),false);
-                for (ILink link2 : links2) {
-                    IInformationObject xdoc = link2.getSourceInformationObject();
-                    InformationObjectType objType = xdoc.getInformationObjectType();
-                    log.info("usage object : " + xdoc.getID() + " /// type : " + objType);
-                    if(objType != InformationObjectType.PROCESS_INSTANCE){continue;}
-                    if(Objects.equals(xdoc.getClassID(), Conf.ClassIDs.ReviewMain)){
-                        decisionCode = "";
-                        IProcessInstance proi = (IProcessInstance) xdoc;
-                        IDocument mainDocument = (IDocument) proi.getMainInformationObject();
-                        if(mainDocument == null){return resultSuccess("No-Main document");}
-                        Collection<ITask> tsks = proi.findTasks();
-                        for(ITask ttsk : tsks) {
-                            if (ttsk.getStatus() != TaskStatus.COMPLETED) {
-                                continue;
-                            }
-                            if(decisionCode != ""){break;}
-                            String tnam = (ttsk.getName() != null ? ttsk.getName() : "");
-                            String tcod = (ttsk.getCode() != null ? ttsk.getCode() : "");
-                            log.info("TASK-Name[" + tnam + "]");
-                            log.info("TASK-Code[" + tcod + "]");
-                            if(ttsk.getLoadedParentTask() != null && (tnam.equals("Consolidator Review") || tcod.equals("Step03"))){
-                                decisionCode = ttsk.getDecision().getCode();
-                                log.info("Approval Code updated.. decisionCode IS:" + decisionCode);
-                                mainDocument.setDescriptorValue("ccmPrjDocApprCode", decisionCode);
-                                mainDocument.commit();
-                                updated.add(mainDocument.getID() + " > " + mainDocument.getDescriptorValue(Conf.Descriptors.DocNumber) + " > " + decisionCode);
-                                log.info("UpdateEngDocument batch...maindoc committed...APPR CODE IS:" + mainDocument.getDescriptorValue("ccmPrjDocApprCode"));
-                                TimeUnit.SECONDS.sleep(30);
-                                log.info("UpdateEngDocument batch...sleeping (30 second)");
-                            }
+            log.info("-- UpdateEngDocuments Agent Started -----");
+            IInformationObject[] list = this.getEmptyDocIDEngineeringDocs(helper);
+            this.updateDocIDEngDocs(list);
+        } catch (Exception e) {
+            log.error("UpdateEngDocuments Exception Caught");
+            log.error(e.getMessage());
+            return resultError(e.getMessage());
+        }
+        log.info("-- UpdateEngDocuments Agent Finished -----");
+        return resultSuccess("Agent Finished Succesfully");
+    }
+    public void updateDecisionCodeEngDocs(IInformationObject[] list) throws Exception {
+        for(IInformationObject infoDoc : list){
+            ILink[] links2 = getDocumentServer().getReferencingRelationships(getSes(),((IDocument) infoDoc).getID(),false);
+            for (ILink link2 : links2) {
+                IInformationObject xdoc = link2.getSourceInformationObject();
+                InformationObjectType objType = xdoc.getInformationObjectType();
+                log.info("usage object : " + xdoc.getID() + " /// type : " + objType);
+                if(objType != InformationObjectType.PROCESS_INSTANCE){continue;}
+                if(Objects.equals(xdoc.getClassID(), Conf.ClassIDs.ReviewMain)){
+                    decisionCode = "";
+                    IProcessInstance proi = (IProcessInstance) xdoc;
+                    IDocument mainDocument = (IDocument) proi.getMainInformationObject();
+                    if(mainDocument == null){
+                        log.info("No-Main document");
+                    }
+                    Collection<ITask> tsks = proi.findTasks();
+                    for(ITask ttsk : tsks) {
+                        if (ttsk.getStatus() != TaskStatus.COMPLETED) {
+                            continue;
+                        }
+                        if(decisionCode != ""){break;}
+                        String tnam = (ttsk.getName() != null ? ttsk.getName() : "");
+                        String tcod = (ttsk.getCode() != null ? ttsk.getCode() : "");
+                        log.info("TASK-Name[" + tnam + "]");
+                        log.info("TASK-Code[" + tcod + "]");
+                        if(ttsk.getLoadedParentTask() != null && (tnam.equals("Consolidator Review") || tcod.equals("Step03"))){
+                            decisionCode = ttsk.getDecision().getCode();
+                            log.info("Approval Code updated.. decisionCode IS:" + decisionCode);
+                            mainDocument.setDescriptorValue("ccmPrjDocApprCode", decisionCode);
+                            mainDocument.commit();
+                            updated.add(mainDocument.getID() + " > " + mainDocument.getDescriptorValue(Conf.Descriptors.DocNumber) + " > " + decisionCode);
+                            log.info("UpdateEngDocument batch...maindoc committed...APPR CODE IS:" + mainDocument.getDescriptorValue("ccmPrjDocApprCode"));
+                            TimeUnit.SECONDS.sleep(30);
+                            log.info("UpdateEngDocument batch...sleeping (30 second)");
                         }
                     }
                 }
             }
-        } catch (Exception e) {
-            log.error("Delete MultiReview Exception Caught");
-            log.error(e.getMessage());
-            return resultError(e.getMessage());
         }
-        log.info("-- Delete MultiReview Agent Finished -----");
-        return resultSuccess("Agent Finished Succesfully");
     }
-
     public IInformationObject[] getEngineeringDocs(ProcessHelper helper) {
         StringBuilder builder = new StringBuilder();
         builder.append("TYPE = '").append(Conf.ClassIDs.EngineeringDocument).append("'")
@@ -87,6 +90,28 @@ public class UpdateEngDocumentsTest extends UnifiedAgent {
                 .append("CCMPRJDOCWFTASKNAME").append(" = '").append("Completed").append("'")
                 .append(" AND ")
                 .append("CCMPRJDOCAPPRCODE").append(" IS NULL");
+        String whereClause = builder.toString();
+        log.info("Where Clause: " + whereClause);
+        IInformationObject[] list = helper.createQuery(new String[]{Conf.Databases.EngineeringDocument}, whereClause, "", 0, false);
+        return list;
+    }
+    public void updateDocIDEngDocs(IInformationObject[] list) throws Exception {
+        for(IInformationObject infoDoc : list){
+            IDocument mainDocument = (IDocument) infoDoc;
+            String docID = mainDocument.getID();
+            log.info("DOC ID updated.. doc ID:" + docID);
+            mainDocument.setDescriptorValue("DocID", docID);
+            mainDocument.commit();
+            log.info("Updated EngDocument batch...maindoc committed...DOC ID IS:" + mainDocument.getDescriptorValue("DocID"));
+            TimeUnit.SECONDS.sleep(30);
+            log.info("UpdateEngDocument batch...sleeping (30 second)");
+        }
+    }
+    public IInformationObject[] getEmptyDocIDEngineeringDocs(ProcessHelper helper) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("TYPE = '").append(Conf.ClassIDs.EngineeringDocument).append("'")
+                .append(" AND ")
+                .append("DOCID").append(" IS NULL");
         String whereClause = builder.toString();
         log.info("Where Clause: " + whereClause);
         IInformationObject[] list = helper.createQuery(new String[]{Conf.Databases.EngineeringDocument}, whereClause, "", 0, false);
